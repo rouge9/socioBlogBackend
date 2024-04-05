4a8bba2178ccc47e69eedce2ae3dcf9f68a1544c
rouge9 <robelged1992@gmail.com>
2024-04-05 08:26:14 -0900

    Fix register route in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,6 @@ import { login, register } from "../controllers/auth.js";
 const router = Router();
 
 router.post("/login", login);
-router.post("/register", register);
+// router.post("/register", register);
 
 export default router;
